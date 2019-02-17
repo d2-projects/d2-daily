cbae7523f593383e2740e251225fabe9032399f9
liyang <1711467488@qq.com>
2019-02-18 07:55:08 +0800

    2.18

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -2,6 +2,7 @@ const fileList = require('../../scripts/file-list.js')
 
 module.exports = {
   title: 'D2 日报',
+  description: '收集开发和设计领域相关资源，服务每位创造行业工作者',
   head: [
     ['meta', { name: 'keywords', content: 'awesome,前端插件,前端开发,vue插件,vue组件,设计资源,程序员网址导航,设计师网址导航' }],
     ['meta', { name: 'description', content: '收集开发和设计领域相关资源，服务每位创造行业工作者' }],
@@ -10,7 +11,9 @@ module.exports = {
     ['link', { rel: 'stylesheet', href: '/library/hint.min.css' }],
     ['link', { rel: 'stylesheet', href: '/library/github-language-colors.css' }],
     // 依赖的脚本
-    ['script', { src: '/library/fontawesome-5.3.1.js' }]
+    ['script', { src: '/library/fontawesome-5.3.1.js' }],
+    // 统计代码
+    ['script', {}, 'var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?fec739493bdcbae03ff2154ede1ed6c7";var s = document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm, s);})();']
   ],
   plugins: [
     [ '@vuepress/search', { searchMaxSuggestions: 10 } ]
@@ -30,6 +33,7 @@ module.exports = {
         ]
       },
       ...[
+        [ '2019', '02' ],
         [ '2019', '01' ],
         [ '2018', '12' ],
         [ '2018', '11' ],
