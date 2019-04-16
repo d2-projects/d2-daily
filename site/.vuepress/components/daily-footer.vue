7ee7688bae77aaed19d5864d3f9144965c1b3327
liyang <1711467488@qq.com>
2019-04-16 23:51:00 +0800

    增加了 d2-admin 的广告内容

--- site/.vuepress/components/daily-footer.vue
+++ site/.vuepress/components/daily-footer.vue
@@ -1,3 +1,21 @@
 <template>
-  <div></div>
+  <div class="daily-footer">
+    <a class="daily-footer--image" href="https://github.com/d2-projects/d2-admin">
+      <img src="https://qiniucdn.fairyever.com/20190416234219.png" alt="d2-admin">
+    </a>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.daily-footer {
+  .daily-footer--image {
+    img {
+      border-radius: 4px;
+      border: 2px solid #eaecef;
+      &:hover {
+        border: 2px solid #2660D0;
+      }
+    }
+  }
+}
+</style>
